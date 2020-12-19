92f711b4840474cc2e9a3f15258b48545a757b0a
Hannah Park <ha3158987@gmail.com>
2020-12-19 19:59:34 +0900

    New: create Person component

---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//react를 import해야지 JSX로 변환이 된다.
+//컴포넌트 폴더/파일 이름은 capital letter로 하는 것이 컨벤션.
+//상수여도 함수이름은 Person이 아닌 person으로 표기.
+const person = () => {
+    return <p>I'm a Person!</p>
+};
+
+export default person;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+//통상적으로 하나의 root컴포넌트를 렌더링하는 방식으로 실행한다.
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

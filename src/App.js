a746b203856a098e60f9d4b28ddb5331115008f2
Hannah Park <ha3158987@gmail.com>
2020-12-22 16:59:00 +0900

    New: create state in App Component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  //state는 자바스크립트 객체. special property
+  state = {
+    persons: [
+      {name: 'Hannah', age: 22},
+      {name: 'Kyeonghee', age: 50},
+      {name: 'Sogeum', age: 3}
+    ]
+  }
+
+  //handler라는 이름을 붙여주면 이벤트 핸들러로 따라오는 함수임을 알 수 있기 때문에 이렇게 짓는 것이 컨벤션
+  switchNameHandler = () => {
+    //console.log("Was clicked!");
+    //DON'T: this.state.persons[0].name = "박현아";
+    this.setState({})
+  }
+
   render() {
     return (
      <div className="App">
        <h1>Hi, I'm a React App</h1>
        <p>This is really working!</p>
-       <Person/>
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Her hobbies: shopping</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
      </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,17 @@ import React from 'react';
 //react를 import해야지 JSX로 변환이 된다.
 //컴포넌트 폴더/파일 이름은 capital letter로 하는 것이 컨벤션.
 //상수여도 함수이름은 Person이 아닌 person으로 표기.
-const person = () => {
-    return <p>I'm a Person!</p>
+//return 뒤에 일반괄호()로 두르게 되면, 여러줄의 JSX를 작성할 수 있다.
+const person = (props) => {
+    return (
+    <div>
+         <p>I'm {props.name} and I am {props.age} years old!</p>
+         <p>{props.children}</p>
+    </div>
+    )
 };
+
+//props는 html에서 attribute을 설정해주는 것과 같다.
+//class에서 props를 가지고 올 때는 'this.props'가 된다.
 
 export default person;
